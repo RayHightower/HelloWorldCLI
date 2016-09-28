5ed9c51c87d84b08907a40a271f86ee53e36a8c9
Raymond T. Hightower <rth@wisdomgroup.com>
2016-09-28 09:51:37 -0500

    Initial commit.

---
+++ tictactoe.cs
@@ -0,0 +1,27 @@
+public class TicTacToe
+{
+
+  public static void DrawHorizontal()
+  {
+    System.Console.WriteLine("--------------");
+  }
+
+  public static void DrawPosts()
+  {
+    System.Console.WriteLine("|   |    |   |");
+  }
+
+  public static void DrawGrid()
+  {
+    DrawPosts();
+    DrawHorizontal;
+    DrawPosts();
+    DrawHorizontal();
+  }
+
+  public static void Main()
+  {
+    System.Console.WriteLine("Hello World!!!!!");
+    DrawGrid();
+  }
+}
