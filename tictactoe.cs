3e9c5d24b435cbf2a378e23736cb7ca8ef176dc5
Raymond T. Hightower <rth@wisdomgroup.com>
2016-09-28 10:10:47 -0500

    Clean up comments and see if I really understand function call syntax.

--- tictactoe.cs
+++ tictactoe.cs
@@ -1,27 +1,23 @@
 public class TicTacToe
 {
-/*
-  public static void DrawHorizontal()
+
+  static void DrawHorizontal()
   {
     System.Console.WriteLine("--------------");
   }
 
-  public static void DrawPosts()
+  static void DrawPosts()
   {
     System.Console.WriteLine("|   |    |   |");
   }
-*/
+
 
   static void DrawGrid()
   {
-    System.Console.WriteLine("|   |    |   |");
-    System.Console.WriteLine("--------------");
-/*
     DrawPosts();
     DrawHorizontal;
     DrawPosts();
     DrawHorizontal();
-*/
   }
 
   public static void Main()
