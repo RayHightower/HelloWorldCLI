f2dc389def3ff2fe313e917db303bbc3f9eb3d94
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-02 16:01:47 -0500

    Bounce back and forth between Xamarin and the command line.

--- helloworld.cs
+++ helloworld.cs
@@ -1,9 +1,12 @@
 using System;
+using NUnit.Framework;
 
 // If I decided to use NUnit in here, where would it have to be stored?
 // Where does the "System" namespace come from when I use it in this fashion?
 // Is the System namespace part of mono.exe?
 // If I want to add NUnit so that I can use it at the command line and within Vim, how do I do this?
+// Fuck it. Use Xamarin. Let Xamarin sort this shit out.
+//
 public class HelloWorld
 {
     public static void Main()
