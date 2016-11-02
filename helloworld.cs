a1afbca545ff96c164032ca0d5e8dcbe8d8cef7c
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-02 13:48:00 -0500

    Fun with namespaces.

--- helloworld.cs
+++ helloworld.cs
@@ -1,7 +1,13 @@
+using System;
+
+// If I decided to use NUnit in here, where would it have to be stored?
+// Where does the "System" namespace come from when I use it in this fashion?
+// Is the System namespace part of mono.exe?
+// If I want to add NUnit so that I can use it at the command line and within Vim, how do I do this?
 public class HelloWorld
 {
     public static void Main()
     {
-      System.Console.WriteLine("Hello World!!!!!");
+      Console.WriteLine("Hello World!!!!!");
     }
 }
